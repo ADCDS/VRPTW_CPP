b14eed84af0b24bff2649986998f9028f5d9c3e7
Adriel Santos <adriel@adriel.eu>
2024-04-08 19:28:52 -0300

    to cmake

--- Utils.cpp
+++ Utils.cpp
@@ -11,185 +11,168 @@
 #include "VehicleState.h"
 #include "Solution.h"
 #include <functional>
+
 #define _USE_MATH_DEFINES
+
 #include <math.h>
 
 std::shared_ptr<VehicleState> Utils::origin;
-std::list<Point*> Utils::raw_rows;
-double** Utils::distances;
+std::list<Point *> Utils::raw_rows;
+double **Utils::distances;
 
-void Utils::read_file()
-{
-	std::ifstream inst;
+void Utils::read_file() {
+    std::ifstream inst;
 
-	inst.open("../Instances/S-RC2-1000/RC21010.TXT");
-	//inst.open("../Instances/C101.TXT");
-	//inst.open("../Instances/RC105.txt");
-	if (inst.is_open())
-	{
-		std::string line;
-		std::regex e("([0-9]+)");
-		std::smatch sm;
-		int line_count = 0;
-		int aux[7];
-		int rr_count = 0;
-		while (std::getline(inst, line))
-		{
-			line_count++;
-			int i = 0;
-			if (line_count > 9)
-			{
-				while (std::regex_search(line, sm, e))
-				{
-					auto s = sm.str();
-					aux[i++] = atoi(s.data());
-					line = sm.suffix();
-				}
-				Utils::raw_rows.push_back(new Point(aux[0], aux[1], aux[2], aux[3], aux[4], aux[5], aux[6]));
-			}
-		}
-		Utils::origin = std::make_shared<VehicleState>(*Utils::raw_rows.begin());		
-		Utils::distances = (double**)malloc(sizeof(double*) * Utils::raw_rows.size());
-		for (int i = 0; i < Utils::raw_rows.size(); ++i)
-		{
-			Utils::distances[i] = (double*)malloc(sizeof(double) * Utils::raw_rows.size());
-		}
-		inst.close();
-	}
-	else
-	{
-		std::cout << "Unable to open file";
-		exit(1);
-	}
+    inst.open("../Instances/RC208.txt");
+    //inst.open("../Instances/S-RC2-1000/RC21010.TXT");
+    //inst.open("../Instances/C101.TXT");
+    //inst.open("../Instances/RC105.txt");
+    if (inst.is_open()) {
+        std::string line;
+        std::regex e("([0-9]+)");
+        std::smatch sm;
+        auto line_count = 0;
+        while (std::getline(inst, line)) {
+            line_count++;
+            if (line_count > 9) // Ignore all lines until the points data
+            {
+                std::vector<uint32_t> aux;
+                aux.reserve(7);
+                while (std::regex_search(line, sm, e)) {
+                    auto s = sm.str();
+                    aux.push_back(static_cast<uint32_t>(strtol(s.data(), nullptr, 10)));
+                    line = sm.suffix();
+                }
+
+                if (aux.size() == 7) { // We expect exactly 7 elements in this array
+                    Utils::raw_rows.push_back(new Point(aux[0], aux[1], aux[2], aux[3], aux[4], aux[5], aux[6]));
+                }
+            }
+        }
+        Utils::origin = std::make_shared<VehicleState>(*Utils::raw_rows.begin());
+        Utils::distances = (double **) malloc(sizeof(double *) * Utils::raw_rows.size());
+        for (int i = 0; i < Utils::raw_rows.size(); ++i) {
+            Utils::distances[i] = (double *) malloc(sizeof(double) * Utils::raw_rows.size());
+        }
+        inst.close();
+    } else {
+        std::cout << "Unable to open file";
+        exit(1);
+    }
 }
 
-void Utils::calculate_distances()
-{
-	for (Point* point : Utils::raw_rows)
-	{
-		for (Point* point2 : Utils::raw_rows)
-		{
-			Utils::distances[point->id][point2->id] = point->distance(point2);
-		}
-	}
+void Utils::calculate_distances() {
+    for (Point *point: Utils::raw_rows) {
+        for (Point *point2: Utils::raw_rows) {
+            Utils::distances[point->id][point2->id] = point->distance(point2);
+        }
+    }
 }
 
-Solution* Utils::pfih()
-{
-	std::vector<std::pair<Point*, double>> points(Utils::raw_rows.size() - 1);
-	auto it = Utils::raw_rows.begin();
-	auto origin = *it;
-	int ec = 0;
-	++it;
+/**
+ * Push forward insert heuristic algorithm
+ * @return
+ */
+Solution *Utils::pfih() {
+    std::vector<std::pair<Point *, double>> points(
+            Utils::raw_rows.size() - 1
+    ); // -1 since we do not count the depot (id == 0 point)
 
-	Point* curr;
-	double degreesdeposit = atan2(origin->y, origin->x) * (180.0 / M_PI);
-	double newdegree = 0;
-	while (it != Utils::raw_rows.end())
-	{
-		curr = *it;
-		newdegree = atan2(curr->y, curr->x) * (180.0 / M_PI);		
-		double d = Utils::distances[0][curr->id];
-		points[curr->id - 1] = std::pair<Point*, double>(curr, (d * -0.7) + (0.1 * curr->due_date) + (0.2 * (newdegree - degreesdeposit) / 360 * d));
-		++it;
-	}
+    auto it = Utils::raw_rows.begin();
+    auto origin = *it;
+    int ec = 0;
+    ++it;
+
+    Point *curr;
+    double degreesdeposit = atan2(origin->y, origin->x) * (180.0 / M_PI);
+    double newdegree = 0;
+    while (it != Utils::raw_rows.end()) {
+        curr = *it;
+        newdegree = atan2(curr->y, curr->x) * (180.0 / M_PI);
+        double d = Utils::distances[0][curr->id];
+        points[curr->id - 1] = std::pair<Point *, double>(curr, (d * -0.7) + (0.1 * curr->due_date) +
+                                                                (0.2 * (newdegree - degreesdeposit) / 360 * d));
+        ++it;
+    }
 
 
-	std::sort(points.begin(), points.end(), Utils::pfih_comparator());
+    std::sort(points.begin(), points.end(), Utils::pfih_comparator());
 
 
-	//Creates a solution
-	auto solution = new Solution();
-	
-	auto v = new Vehicle(solution);
-	v->id = 0;
-	solution->vehicles.push_back(v);
-	v->add_node(points[0].first, 0, ec);
-	std::list<SubCandidate> candidates;
+    //Creates a solution
+    auto solution = new Solution();
 
-	for (auto it = ++points.begin(); it != points.end(); ++it)
-	{
-		auto point = *it;
-		for (auto vit = solution->vehicles.begin(); vit != solution->vehicles.end(); ++vit)
-		{
-			auto vehicle = *vit;
-			for (int i = 0; i <= vehicle->nodes.size(); i++)
-			{
-				ec = 0;
-				vehicle->add_node(point.first, i, ec);
-				if (ec == 0)
-				{
-					SubCandidate sb(solution->total_weight(), i, vehicle);
-					candidates.push_back(sb);
-					vehicle->remove_node(i);
-				}
-			}
-		}
+    auto v = new Vehicle(solution);
+    v->id = 0;
+    solution->vehicles.push_back(v);
+    v->add_node(points[0].first, 0, ec);
+    std::list<SubCandidate> candidates;
 
-		if (!candidates.empty())
-		{
-			candidates.sort();
-			auto chosen_one = *(candidates.begin());
-			ec = 0;
-			chosen_one.v->add_node(point.first, chosen_one.pos, ec);
-			point.first->state.insert({ solution, std::pair<Vehicle*, int>(chosen_one.v, chosen_one.pos) });
-			candidates.clear();
-		}
-		else
-		{
-			auto nv = new Vehicle(solution);
-			nv->id = solution->vehicles.size();
-			solution->vehicles.push_back(nv);
-			ec = 0;
-			nv->add_node(point.first, 0, ec);
-			point.first->state.insert({ solution, std::pair<Vehicle*, int>(nv, 0) });
-		}
-	}
+    for (auto it = ++points.begin(); it != points.end(); ++it) {
+        auto point = *it;
+        for (auto vit = solution->vehicles.begin(); vit != solution->vehicles.end(); ++vit) {
+            auto vehicle = *vit;
+            for (int i = 0; i <= vehicle->nodes.size(); i++) {
+                ec = 0;
+                vehicle->add_node(point.first, i, ec);
+                if (ec == 0) {
+                    SubCandidate sb(solution->total_weight(), i, vehicle);
+                    candidates.push_back(sb);
+                    vehicle->remove_node(i);
+                }
+            }
+        }
 
-	return solution;
+        if (!candidates.empty()) {
+            candidates.sort();
+            auto chosen_one = *(candidates.begin());
+            ec = 0;
+            chosen_one.v->add_node(point.first, chosen_one.pos, ec);
+            point.first->state.insert({solution, std::pair<Vehicle *, int>(chosen_one.v, chosen_one.pos)});
+            candidates.clear();
+        } else {
+            auto nv = new Vehicle(solution);
+            nv->id = solution->vehicles.size();
+            solution->vehicles.push_back(nv);
+            ec = 0;
+            nv->add_node(point.first, 0, ec);
+            point.first->state.insert({solution, std::pair<Vehicle *, int>(nv, 0)});
+        }
+    }
+
+    return solution;
 }
 
-Solution* Utils::random_solution()
-{
-	auto solution = new Solution;
-	auto v = new Vehicle(solution);
-	solution->vehicles.push_back(v);
+Solution *Utils::random_solution() {
+    auto solution = new Solution;
+    auto v = new Vehicle(solution);
+    solution->vehicles.push_back(v);
 
-	auto point_pool = Utils::raw_rows;
-	point_pool.erase(point_pool.begin());
-	while(!point_pool.empty())
-	{
-		auto pit = point_pool.begin();
-		std::advance(pit, rand() % point_pool.size());
-		auto p = *pit;
-		point_pool.erase(pit);
-		auto ec = 0;
-		for (auto vehicle : solution->vehicles)
-		{
-			for (auto i = 0; i <= vehicle->nodes.size(); ++i)
-			{			
-				ec = 0;
-				vehicle->add_node(p, i, ec);
-				if (ec == 0)
-					goto leave;
-			}			
-		}
+    auto point_pool = Utils::raw_rows;
+    point_pool.erase(point_pool.begin());
+    while (!point_pool.empty()) {
+        auto pit = point_pool.begin();
+        std::advance(pit, rand() % point_pool.size());
+        auto p = *pit;
+        point_pool.erase(pit);
+        auto ec = 0;
+        for (auto vehicle: solution->vehicles) {
+            for (auto i = 0; i <= vehicle->nodes.size(); ++i) {
+                ec = 0;
+                vehicle->add_node(p, i, ec);
+                if (ec == 0)
+                    goto leave;
+            }
+        }
 
-		if(ec != 0)
-		{
-			v = new Vehicle(solution);
-			solution->vehicles.push_back(v);
-			auto ec = 0;
-			v->add_node(p, 0, ec);
-		}
-	leave:;
-	}
-	solution->total_weight();
-	return solution;
-}
-
-
-double Utils::random_0_1()
-{
-	return ((double)rand() / (RAND_MAX));
+        if (ec != 0) {
+            v = new Vehicle(solution);
+            solution->vehicles.push_back(v);
+            auto ec = 0;
+            v->add_node(p, 0, ec);
+        }
+        leave:;
+    }
+    solution->total_weight();
+    return solution;
 }
--- Vehicle.cpp
+++ Vehicle.cpp
@@ -16,237 +16,203 @@
 		}
 	}
 }*/
-void Vehicle::reevalueate_route(int start, int end)
-{
-	std::list<std::shared_ptr<VehicleState>>::iterator li;
-	std::shared_ptr<VehicleState> last;	
-	//start = 0;
-	//end = nodes.size();
-	
-	if (start == 0)
-	{
-		li = nodes.begin();
-		last = Utils::origin;		
-	}
-	else
-	{
-		li = nodes.begin();		
-		int tmp_start = start;
-		while (tmp_start-- > 0)
-			++li;
-		last = *std::prev(li);
-		
-	}
-	std::shared_ptr<VehicleState> curr;
-	while (++start <= end && li != nodes.end())
-	{
-		curr = *li;
-		li++;
-		curr->current_cargo = last->current_cargo - curr->p->demand;
-		curr->current_distance = last->current_distance + Utils::distances[last->p->id][curr->p->id];
-		curr->current_time = last->current_time + Utils::distances[last->p->id][curr->p->id];
-		curr->p->state[solution].second = start - 1;
+void Vehicle::reevaluate_route(int start, int end) {
+    std::list<std::shared_ptr<VehicleState>>::iterator li;
+    std::shared_ptr<VehicleState> last;
+    //start = 0;
+    //end = nodes.size();
 
-		if (curr->current_time < curr->p->ready_time)
-		{
-			curr->current_time = curr->p->ready_time;
-		}
-		curr->current_time += curr->p->service_time;
+    if (start == 0) {
+        li = nodes.begin();
+        last = Utils::origin;
+    } else {
+        li = nodes.begin();
+        int tmp_start = start;
+        while (tmp_start-- > 0)
+            ++li;
+        last = *std::prev(li);
 
-		last = curr;
-	}	
+    }
+    std::shared_ptr<VehicleState> curr;
+    while (++start <= end && li != nodes.end()) {
+        curr = *li;
+        li++;
+        curr->current_cargo = last->current_cargo - curr->p->demand;
+        curr->current_distance = last->current_distance + Utils::distances[last->p->id][curr->p->id];
+        curr->current_time = last->current_time + Utils::distances[last->p->id][curr->p->id];
+        curr->p->state[solution].second = start - 1;
+
+        if (curr->current_time < curr->p->ready_time) {
+            curr->current_time = curr->p->ready_time;
+        }
+        curr->current_time += curr->p->service_time;
+
+        last = curr;
+    }
 }
 
-bool Vehicle::is_point_on_vehicle(Point* p)
-{
-	for (auto value_ : nodes)
-	{
-		if (value_->p->id == p->id)
-		{
-			return true;
-		}
-	}
-	return false;
+bool Vehicle::is_point_on_vehicle(Point *p) {
+    for (auto value_: nodes) {
+        if (value_->p->id == p->id) {
+            return true;
+        }
+    }
+    return false;
 }
 
-void Vehicle::validate_states(std::shared_ptr<VehicleState> possible_state, int position, int &ec)
-{
-	int c_position = position;
-	
-	if (!nodes.empty())
-	{
-		std::list<std::shared_ptr<VehicleState>>::iterator li;
-		std::shared_ptr<VehicleState> last_state;
-		std::shared_ptr<VehicleState> current_state;
-		int last_state_position;
-		if (position == 0)
-		{
-			li = nodes.begin();
-			last_state = Utils::origin;
-			last_state_position = -1;
-		}else
-		{
-			li = nodes.begin();
-			int tmp_position = position;
-			while (tmp_position-- > 0)
-				++li;
+void Vehicle::validate_states(std::shared_ptr<VehicleState> possible_state, int position, int &ec) {
+    if (!nodes.empty()) {
+        std::list<std::shared_ptr<VehicleState>>::iterator li;
+        std::shared_ptr<VehicleState> last_state;
 
-			last_state = *std::prev(li);
-			last_state_position = last_state->p->state[solution].second;
-		}
-		current_state = possible_state;
-		int iter_num = position;
-		double dist;
-		do
-		{
-			dist = Utils::distances[last_state->p->id][current_state->p->id];
-			current_state->current_distance = last_state->current_distance + dist;
-			current_state->current_time = last_state->current_time + dist;
+        int last_state_position;
+        if (position == 0) {
+            li = nodes.begin();
+            last_state = Utils::origin;
+            last_state_position = -1;
+        } else {
+            li = nodes.begin();
+            int tmp_position = position;
+            while (tmp_position-- > 0)
+                ++li;
 
-			if(current_state->current_time > current_state->p->due_date)
-			{
-				if(iter_num > 0)
-				{
-					reevalueate_route(position, iter_num);
-				}
-				ec = 1;
-				return;
-			}
+            last_state = *std::prev(li);
+            last_state_position = last_state->p->state[solution].second;
+        }
+        int iter_num = position;
+        double dist;
 
-			current_state->current_cargo = last_state->current_cargo - current_state->p->demand;
+        std::shared_ptr<VehicleState> current_state;
+        for (; li != nodes.end(); ++li) {
+            current_state = *li;
+            dist = Utils::distances[last_state->p->id][current_state->p->id];
+            current_state->current_distance = last_state->current_distance + dist;
+            current_state->current_time = last_state->current_time + dist;
 
-			if(current_state->current_time < current_state->p->ready_time)
-			{
-				current_state->current_time = current_state->p->ready_time;
-			}
-			if(current_state->current_cargo < 0)
-			{
-				if (iter_num > 0)
-				{
-					reevalueate_route(position, iter_num);
-				}
-				ec = 4;
-				return;
-			}
+            if (current_state->current_time > current_state->p->due_date) {
+                if (iter_num > 0) {
+                    reevaluate_route(position, iter_num);
+                }
+                ec = 1;
+                return;
+            }
 
-			current_state->current_time += current_state->p->service_time;
-			current_state->p->state[solution].second = last_state_position + 1;
-			last_state_position = current_state->p->state[solution].second;
-			last_state = current_state;			
-			iter_num++;
-		} while (li != nodes.end());
-		last_node = current_state;
-	}
-	else
-	{
-		possible_state->current_distance = Utils::distances[Utils::origin->p->id][possible_state->p->id];
-		possible_state->current_time = possible_state->current_distance;
-		if (possible_state->current_time > possible_state->p->due_date)
-		{
-			ec = 2;
-			return;
-		}
-		if (possible_state->current_time < possible_state->p->ready_time)
-		{
-			possible_state->current_time = possible_state->p->ready_time;
-		}
-		possible_state->current_cargo -= possible_state->p->demand;
-		possible_state->current_time += possible_state->p->service_time;
-		possible_state->p->state[solution].second = 0;
-		last_node = possible_state;
-	}
+            current_state->current_cargo = last_state->current_cargo - current_state->p->demand;
 
-	if (last_node->current_time + Utils::distances[last_node->p->id][Utils::origin->p->id] > Utils::origin->p->due_date)
-	{
-		ec = 3;
-		return;
-	}
-	
+            if (current_state->current_time <
+                current_state->p->ready_time) { // Arrived early, need to wait for the point to become active
+                current_state->current_time = current_state->p->ready_time;
+            }
+            if (current_state->current_cargo < 0) {
+                if (iter_num > 0) {
+                    reevaluate_route(position, iter_num);
+                }
+                ec = 4;
+                return;
+            }
+
+            current_state->current_time += current_state->p->service_time;
+            current_state->p->state[solution].second = last_state_position + 1;
+            last_state_position = current_state->p->state[solution].second;
+            last_state = current_state;
+            iter_num++;
+        }
+    } else {
+        possible_state->current_distance = Utils::distances[Utils::origin->p->id][possible_state->p->id];
+        possible_state->current_time = possible_state->current_distance;
+        if (possible_state->current_time > possible_state->p->due_date) {
+            ec = 2;
+            return;
+        }
+        if (possible_state->current_time < possible_state->p->ready_time) {
+            possible_state->current_time = possible_state->p->ready_time;
+        }
+        possible_state->current_cargo -= possible_state->p->demand;
+        possible_state->current_time += possible_state->p->service_time;
+        possible_state->p->state[solution].second = 0;
+    }
+
+    if(this->nodes.empty())
+        return; // I guess it's safe to simply return here if there are no nodes
+
+    if (get_last_node()->current_time + Utils::distances[get_last_node()->p->id][Utils::origin->p->id] >
+        Utils::origin->p->due_date) {
+        ec = 3;
+        return;
+    }
+
 }
 
-void Vehicle::add_node(Point* p, int pos, int &ec)
-{
+void Vehicle::add_node(Point *p, int pos, int &ec) {
 
-	auto possible_state = std::make_shared<VehicleState>(p);
-	validate_states(possible_state, pos, ec);
-	if(ec)			
-		return;
+    auto possible_state = std::make_shared<VehicleState>(p);
+    validate_states(possible_state, pos, ec);
+    if (ec)
+        return;
 
-	auto it = nodes.begin();
-	auto tmp_pos = pos;
-	while(tmp_pos-- > 0)
-		++it;
+    auto it = nodes.begin();
+    auto tmp_pos = pos;
+    while (tmp_pos-- > 0)
+        ++it;
 
-	nodes.insert(it, possible_state);
-	p->state[solution].first = this;	
+    nodes.insert(it, possible_state);
+    p->state[solution].first = this;
 }
 
-void Vehicle::remove_node(int pos)
-{
-	std::list<std::shared_ptr<VehicleState>>::iterator li = nodes.begin();
-	
-	std::shared_ptr<VehicleState> last;
-	
-	int c_pos = pos;
-	while (c_pos-- > 0)
-		++li;
-	
-	nodes.erase(li++);
+void Vehicle::remove_node(int pos) {
+    auto li = nodes.begin();
 
-	if (pos == 0)
-	{		
-		last = Utils::origin;
-	}
-	else
-	{				
-		last = *std::prev(li);
-	}
+    std::shared_ptr<VehicleState> last;
 
-	std::shared_ptr<VehicleState> curr;
-	while (li != nodes.end())
-	{
-		curr = *li;
-		curr->current_cargo = last->current_cargo - curr->p->demand;
-		curr->current_distance = last->current_distance + Utils::distances[last->p->id][curr->p->id];
-		curr->current_time = last->current_time + Utils::distances[last->p->id][curr->p->id];
+    int c_pos = pos;
+    while (c_pos-- > 0)
+        ++li;
 
-		if (curr->current_time < curr->p->ready_time)
-		{
-			curr->current_time = curr->p->ready_time;
-		}
-		curr->current_time += curr->p->service_time;
+    if (pos == 0) {
+        last = Utils::origin;
+    } else {
+        last = *std::prev(li);
+    }
 
-		curr->p->state[solution].second = curr->p->state[solution].second - 1;
-		last = curr;
-		++li;
-	}
-	last_node = last;	
+    nodes.erase(li++);
+
+    std::shared_ptr<VehicleState> curr;
+    while (li != nodes.end()) {
+        curr = *li;
+        curr->current_cargo = last->current_cargo - curr->p->demand;
+        curr->current_distance = last->current_distance + Utils::distances[last->p->id][curr->p->id];
+        curr->current_time = last->current_time + Utils::distances[last->p->id][curr->p->id];
+
+        if (curr->current_time < curr->p->ready_time) {
+            curr->current_time = curr->p->ready_time;
+        }
+        curr->current_time += curr->p->service_time;
+
+        curr->p->state[solution].second = curr->p->state[solution].second - 1;
+        last = curr;
+        ++li;
+    }
 }
 
 double Vehicle::get_weight()
-const
-{
-	return last_node->current_distance + Utils::distances[last_node->p->id][Utils::origin->p->id];
+const {
+    return this->get_last_node()->current_distance + Utils::distances[get_last_node()->p->id][Utils::origin->p->id];
 }
 
-Vehicle::Vehicle(Solution *s)
-{
-	solution = s;
+Vehicle::Vehicle(Solution *s) {
+    solution = s;
 }
 
 
-Vehicle* Vehicle::clone(Solution* s)
-{
-	auto v = new Vehicle(s);
-	v->id = this->id;
-	std::shared_ptr<VehicleState> ln = nullptr;
-	for (auto node : this->nodes)
-	{
-		auto clone = node->clone();
-		ln = clone;
-		clone->p->state[s] = {v, node->p->state[this->solution].second};
-		v->nodes.push_back(clone);
-	}
-	v->last_node = ln;	
-	return v;
+Vehicle *Vehicle::clone(Solution *s) {
+    auto v = new Vehicle(s);
+    v->id = this->id;
+    std::shared_ptr<VehicleState> ln = nullptr;
+    for (auto node: this->nodes) {
+        auto clone = node->clone();
+        ln = clone;
+        clone->p->state[s] = {v, node->p->state[this->solution].second};
+        v->nodes.push_back(clone);
+    }
+    return v;
 }
